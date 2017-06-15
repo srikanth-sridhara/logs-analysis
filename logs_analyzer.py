cc6e889124aece8ead180f2a6fd73bfe98bfb55c
Srikanth Sridhara <metalsriks@gmail.com>
2017-06-15 00:35:11 -0700

    made modifications to improve DB connection and making DB do more stuff in query 3

--- logs_analyzer.py
+++ logs_analyzer.py
@@ -2,12 +2,28 @@
 import psycopg2
 from datetime import datetime
 
-# Connect to the DB and get a cursor
-connection = psycopg2.connect("dbname=news")
-cursor = connection.cursor()
+
+# Function that connects to the DB and executes a query
+def connect_and_execute(query):
+    try:
+        # Connect to the DB and get a cursor
+        connection = psycopg2.connect("dbname=news")
+        # Get a cursor
+        cursor = connection.cursor()
+        # Execute the query
+        cursor.execute(query)
+        # Get the results
+        results = cursor.fetchall()
+        # Close the connection
+        connection.close()
+        return results
+    except:
+        print("ERROR in DB connection")
+        return None
+
 
 # 1) Query to get the 3 most popular articles of all time
-cursor.execute("""
+article_results = connect_and_execute("""
     SELECT SUB1.title,SUB2.num FROM (
         SELECT title,'/article/' || slug AS path
         FROM articles
@@ -19,12 +35,12 @@ cursor.execute("""
         ORDER BY num DESC LIMIT 3
     ) SUB2
     ON SUB1.path = SUB2.path
-    ORDER BY SUB2.num DESC;
+    ORDER BY SUB2.num DESC
 """)
-article_results = cursor.fetchall()
+
 
 # 2) Query to get the most popular authors with their total views
-cursor.execute("""
+author_results = connect_and_execute("""
     SELECT name,sum(views) AS total FROM (
         SELECT authors.name,'/article/'|| articles.slug AS path FROM
         articles JOIN authors
@@ -38,58 +54,52 @@ cursor.execute("""
     WHERE name IS NOT NULL
     GROUP BY SUB1.name ORDER BY total DESC
 """)
-author_results = cursor.fetchall()
+
 
 # 3) Query to find out on which day > 1% of requests led to errors
-cursor.execute("""
-    SELECT time::DATE, status, count(*) AS num_requests
-    FROM log GROUP BY time::DATE,status;
+log_results = connect_and_execute("""
+    SELECT * FROM (
+        SELECT tbl1.time::DATE,
+        round((100*tbl1.num_errors::decimal/tbl1.num_total),2)
+        AS percentage FROM (
+            SELECT time::DATE, count(*) AS num_total,
+            sum(CASE WHEN status != '200 OK' THEN 1 ELSE 0 END)
+            AS num_errors
+            FROM log GROUP BY time::DATE
+        ) AS tbl1
+    ) AS tbl2
+    WHERE percentage > 1
 """)
-log_results = cursor.fetchall()
 
-# Close the connection
-connection.close()
 
 # Print the results
 print "================================================="
 print "1) Most popular 3 articles of all time:"
 print "================================================="
-for result in article_results:
-    title, views = result[0], result[1]
-    print "\"%s\" - %s views" % (title, views)
+if article_results is not None:
+    for result in article_results:
+        title, views = result[0], result[1]
+        print "\"%s\" - %s views" % (title, views)
 
 print "================================================="
 print "\n"
 print "================================================="
 print "2) Most popular authors:"
 print "================================================="
-for result in author_results:
-    author, views = result[0], result[1]
-    print "%s - %s views" % (author, views)
+if author_results is not None:
+    for result in author_results:
+        author, views = result[0], result[1]
+        print "%s - %s views" % (author, views)
 
 print "================================================="
 print "\n"
 print "================================================="
 print "3) Days on which > 1% of requests led to errors:"
 print "================================================="
-total = {}
-bad = {}
-dates = []
-for result in log_results:
-    date, status, num_requests = result[0], result[1], result[2]
-    if str(date) not in dates:
-        dates.append(str(date))
-    if '200' not in status:
-        bad[str(date)] = int(num_requests)
-    if str(date) not in total:
-        total[str(date)] = int(num_requests)
-    else:
-        total[str(date)] += int(num_requests)
-
-for single_date in dates:
-    percentage = 100 * float(bad[single_date])/float(total[single_date])
-    if percentage > 1:
-        datestring = datetime.strptime(single_date, "%Y-%m-%d")
+if log_results is not None:
+    for result in log_results:
+        date, percentage = result[0], result[1]
+        datestring = datetime.strptime(str(date), "%Y-%m-%d")
         datestring = datestring.date().strftime("%B %d, %Y")
         print "%s - %.2f%% errors" % (datestring, percentage)
 print "================================================="
